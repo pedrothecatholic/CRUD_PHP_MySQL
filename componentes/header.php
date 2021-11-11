d4e6fc888cd1d2e8f8cc92c9b09c0d756c7f23ba
Pedro Henrique de Sousa <pedrozica.phs21@gmail.com>
2021-11-11 11:23:32 -0300

    Indentação...

--- componentes/header.php
+++ componentes/header.php
@@ -1,5 +1,6 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
 
     <meta charset="UTF-8">
@@ -9,37 +10,37 @@
     <title>SISTEMA DE CADASTRO</title>
 
     <link rel="stylesheet" href="../bootstrap/css/bootstrap.css">
-    <link rel="stylesheet" href="../bootstrap/css/style.css">    
+    <link rel="stylesheet" href="../bootstrap/css/style.css">
 
 </head>
 
 <body>
-    
-<nav class="navbar navbar-dark bg-primary">
-    <a class="navbar-brand" href="">
-        Cadastro
-    </a>
 
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="../cadastro">Cadastrar</a>
-        </li>
+    <nav class="navbar navbar-dark bg-primary">
+        <a class="navbar-brand" href="">
+            Cadastro
+        </a>
 
-    </ul>
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="../cadastro">Cadastrar</a>
+            </li>
 
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="../listagem">Listar</a>
-        </li>
-        
-    </ul>
+        </ul>
 
-    <ul class="navbar-nav mr-auto">
-        <li class="nav-item">
-            <a class="nav-link" href="../login/">Sair</a>
-        </li>
-        
-    </ul>
-    
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="../listagem">Listar</a>
+            </li>
 
-</nav>
+        </ul>
+
+        <ul class="navbar-nav mr-auto">
+            <li class="nav-item">
+                <a class="nav-link" href="../login/">Sair</a>
+            </li>
+
+        </ul>
+
+
+    </nav>
--- listagem/index.php
+++ listagem/index.php
@@ -1,25 +1,25 @@
 <?php
-    include('../componentes/header.php');
+include('../componentes/header.php');
 ?>
 
 <div class="container">
 
-    <br/>
-    
+    <br />
+
     <table class="table table-bordered">
 
-    <thead>
-        <tr>
-            <th>ID</th>
-            <th>Nome</th>
-            <th>Sobrenome</th>
-            <th>E-mail</th>
-            <th>Celular</th>
-            <th>Ações</th>
-        </tr>
-    </thead>
+        <thead>
+            <tr>
+                <th>ID</th>
+                <th>Nome</th>
+                <th>Sobrenome</th>
+                <th>E-mail</th>
+                <th>Celular</th>
+                <th>Ações</th>
+            </tr>
+        </thead>
 
-    <tbody>
+        <tbody>
             <tr>
                 <th>1</th>
                 <th>TESTE DE NOME</th>
@@ -27,21 +27,25 @@
                 <th>TESTE DE EMAIL</th>
                 <th>TESTE DE CELULAR</th>
                 <th>
-                    <button class="btn btn-warning">Editar</button>
 
-                    <form action="" method="post" style="display: inline;">
+                    <a href="editar.php?cod_pessoa=<?php echo $dados["cod_pessoa"]?>">EDITAR</a>
+                    <a href="acoes.php?cod_pessoa=<?php echo $dados["cod_pessoa"].'&acoes=delete'?>">EXCLUIR</a>
+                    
+                    <!-- <button class="btn btn-warning">Editar</button> -->
+                    <!-- 
+                        <form action="" method="post" style="display: inline;">
                         <input type="hidden" name="id" value="">
                         <button class="btn btn-danger">Excluir</button>
-                    </form>
-                    
+                    </form> -->
+
                 </th>
             </tr>
-    </tbody>
+        </tbody>
 
     </table>
 
 </div>
 
 <?php
-    include('../componentes/footer.php');
+include('../componentes/footer.php');
 ?>
